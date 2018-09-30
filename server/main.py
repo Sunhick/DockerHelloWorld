9072de58528430fa34205bb9015a4c97cc5ad742
Sunil <sunhick@gmail.com>
2018-09-29 17:38:23 -0700

    working websocket server and client

---
+++ server/main.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+# websocket server in python
+
+import asyncio
+import websockets
+
+async def hello(websocket, path):
+    name = await websocket.recv()
+    print(f"< {name}")
+
+    greeting = f"Hello {name}!"
+
+    await websocket.send(greeting)
+    print(f"> {greeting}")
+
+start_server = websockets.serve(hello, 'localhost', 8765)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
+
+# #!/usr/bin/env python
+
+# # WS server that sends messages at random intervals
+
+# import asyncio
+# import datetime
+# import random
+# import websockets
+
+# async def time(websocket, path):
+#     while True:
+#         now = datetime.datetime.utcnow().isoformat() + 'Z'
+#         await websocket.send(now)
+#         await asyncio.sleep(random.random() * 3)
+
+# start_server = websockets.serve(time, '127.0.0.1', 5678)
+
+# asyncio.get_event_loop().run_until_complete(start_server)
+# asyncio.get_event_loop().run_forever()
